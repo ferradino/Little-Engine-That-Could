481801e4155f70bd7d40227a4ef852ec33ec596e
Nicholas Ferradino <nferradino@gmail.com>
2022-07-14 11:12:59 -0400

    Little Engine Commit

--- littleengine.cpp
+++ littleengine.cpp
@@ -8,7 +8,7 @@ int main() {
     // Step 1: Define variables 
     
     int 
-        tractiveEffort, // Power of the train
+        tractiveEffort,         // Power of the train
         numLumber, 
         numBeams,
         numLumberFlatcars,
@@ -22,8 +22,8 @@ int main() {
         BEAM_CAPACITY = 3;      // Max beams per flatcar
     
     double 
-        maxLoad,     // Max weight the train can move 
-        rulingGrade, // Slope of track
+        maxLoad,                // Max weight the train can move 
+        rulingGrade,            // Slope of track
         weightOfLumber,
         weightOfBeams,
         weightOfLumberFlatcars,
@@ -33,9 +33,9 @@ int main() {
         weightOfConsist;
     
     const double 
-        KG_TO_LBS = 2.204, // Conversion value for kg to lbs
-        R1 = 0.004,        // Resistance due to friction of moving parts
-        R2 = 0.01;         // Resistance due to incline
+        KG_TO_LBS = 2.204,      // Conversion value for kg to lbs
+        R1 = 0.004,             // Resistance due to friction of moving parts
+        R2 = 0.01;              // Resistance due to incline
 
     // Step 2: Get input from user
     
@@ -52,13 +52,23 @@ int main() {
     cout << "Enter ruling grade (2.5% = 2.5): ";
     cin >> rulingGrade;
 
-    // Validate data from user (inputs cannot be negative)
-    
+    // Step 2.1: Validate input data from user
+    if (numBeams < 0 || numLumber < 0) {
+        cout << "Error: Units of beams and/or lumber must be zero or greater" << endl;
+        return 1;
+    }
+
+    if (rulingGrade < 0){
+        cout << "Error: Ruling grade must be zero or greater" << endl;
+        return 1;
+    }
 
     // Step 3: Calculate max load
     
     maxLoad = tractiveEffort / (R1 + rulingGrade * R2);
     
+    // Step 4: Calculate weight of train
+
     // Step 4.1: Calculate weight of lumber
     
     weightOfLumber = numLumber * LUMBER_MASS * KG_TO_LBS;
@@ -69,16 +79,16 @@ int main() {
     
     // Step 4.3: Calculate weight of flatcars
     
-    numBeamFlatcars = (numBeams + BEAM_CAPACITY - 1) / BEAM_CAPACITY;        // ceil(n/d) for beam cars
-    numLumberFlatcars = (numLumber + LUMBER_CAPACITY - 1) / LUMBER_CAPACITY; // ceil(n/d) for lumber cars
+    numBeamFlatcars = (numBeams + BEAM_CAPACITY - 1) / BEAM_CAPACITY;                    // ceil(n/d) for beam cars
+    numLumberFlatcars = (numLumber + LUMBER_CAPACITY - 1) / LUMBER_CAPACITY;             // ceil(n/d) for lumber cars
 
-    weightOfFlatcars = (numBeamFlatcars + numLumberFlatcars) * FLATCAR_MASS * KG_TO_LBS;
+    weightOfFlatcars = (numBeamFlatcars + numLumberFlatcars) * FLATCAR_MASS * KG_TO_LBS; 
     
     // Step 4.4: Calculate weight of train
     
     weightOfConsist = weightOfLocomotive + weightOfLumber + weightOfBeams + weightOfFlatcars;
 
-    // Step 5: Output results
+    // Step 5: Output results and determine if train can carry load
     
     cout << endl;
     cout << setprecision(10);
@@ -87,9 +97,7 @@ int main() {
     cout << "Number of beam cars: " << numBeamFlatcars << endl;
     cout << "Consist weight: " << weightOfConsist << " lbs" << endl;
     cout << "Maximum load: " << maxLoad << " lbs" << endl;
-    
-    // Determine whether train can carry laod
-    
+   
     if (weightOfConsist <= maxLoad) {   // Train can carry load
         cout << "The locomotive can move the consist along the grade" << endl;
 
